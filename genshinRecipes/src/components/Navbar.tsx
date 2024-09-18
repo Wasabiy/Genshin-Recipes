e0dac3db60e4e4180a3d37f274b6cb7d7481971c
Jayan Thayananthan <jayant@stud.ntnu.no>
2024-09-18 19:16:06 +0200

    feat: added navbar
    added a navbar and made sure to style it a tiny bit
    
    Authored by: Jayan Thayananthan <jayant@stud.ntnu.no>

--- genshinRecipes/src/components/Navbar.tsx
+++ genshinRecipes/src/components/Navbar.tsx
@@ -1,23 +1,37 @@
 import React from "react";
 import "./Navbar.css";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Navbar: React.FC = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-logo"><Link to=""><img src="../src/assets/logo.png" alt="logo" />Wanmin Restaurant</Link></div>
+      <div className="navbar-logo">
+        <Link to="">
+         The Wanmin Restaurant
+        </Link>
+      </div>
       <ul className="navbar-links">
         <li>
-          <img src="../src/assets/primogem.png" alt="primo" id="textLogo" />
-          <Link to="/recipes">Recipes</Link>
+          <Link to="/recipes">
+            <img
+              src="../src/assets/genshinFood.png"
+              alt="primo"
+              id="textLogo"
+            />
+            Recipes
+          </Link>
         </li>
         <li>
-          <img src="../src/assets/slime.png" alt="primo" id="textLogo" />
-          <Link to="/favorites">My Favorites</Link>
+          <Link to="/favorites">
+            <img src="../src/assets/star.png" alt="primo" id="textLogo" />
+            My Favorites
+          </Link>
         </li>
         <li>
-          <img src="../src/assets/paimon.png" alt="paimon" />
-          <Link to="/about">About</Link>
+          <Link to="/about">
+            <img src="../src/assets/paimon.png" alt="paimon" />
+            About
+          </Link>
         </li>
       </ul>
     </nav>
