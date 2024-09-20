766a2d7cf53b92c17911f776afe5a5ad7ba3be92
Jayan Thayananthan <jayant@stud.ntnu.no>
2024-09-20 02:35:40 +0200

    feat: styled navbar and homePage
    Style the navbar to match homePage and also add styling and other features to mainpage. Also change index.css accordingly.
    
    Co-authored-by: Selin Yuki <selinyo@stud.ntnu.no>

--- genshinRecipes/src/pages/HomePage.tsx
+++ genshinRecipes/src/pages/HomePage.tsx
@@ -1,10 +1,27 @@
-function HomePage() {
-    return (
-        <>
-            <h1>Home</h1>
-        </>
-    )
-    
+import homePageMovie from "../assets/homepage.mp4";
+import "./HomePage.css";
+import { useNavigate } from "react-router-dom";
 
+function HomePage() {
+  const navigateRecipes = useNavigate();
+
+  const handleClick = (e: { stopPropagation: () => void; }) => {
+    e.stopPropagation();
+    navigateRecipes("/recipes");
+  };
+
+  return (
+    <>
+    <section id="WholePage">
+      <video loop autoPlay muted>
+        <source src={homePageMovie} type="video/mp4" />
+      </video>
+    <section id="homeTitle">
+        <h1 id="wanminTitle" onClick={handleClick}>Welcome to Wanmin Restaurant. Press to see more...</h1>
+    </section>
+
+    </section>
+    </>
+  );
 }
 export default HomePage;
