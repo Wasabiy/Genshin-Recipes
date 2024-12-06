88a76f6163f68f088cf4f66b0ea18f396c9f9c7d
Wahib <syedh@stud.ntnu.no>
2024-12-06 18:26:22 +0100

    feat: quick fix branched correctly

--- genshinRecipes/src/main.tsx
+++ genshinRecipes/src/main.tsx
@@ -4,10 +4,10 @@ import App from './App.tsx';
 import './index.css';
 import { HashRouter } from 'react-router-dom';
 
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+
     <HashRouter>
       <App />
     </HashRouter>
-  </StrictMode>,
 );
